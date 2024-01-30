93949cec293b7534e3bb9c790a496b4330dd007d
Radek Pribyl <radek.pribyl@seznam.cz>
2024-01-30 22:56:42 +0100

    KX-11274 - Initial commit for the OpenAI Azure integration repository

---
+++ src/Kentico.Xperience.OpenAI.Azure/Module/AzureOpenAIModule.cs
@@ -0,0 +1,25 @@
+﻿using CMS;
+using CMS.DataEngine;
+
+using Kentico.Xperience.OpenAI.Azure;
+
+[assembly: RegisterModule(typeof(AzureOpenAIModule))]
+
+namespace Kentico.Xperience.OpenAI.Azure
+{
+    /// <summary>
+    /// Represents the Azure OpenAI module for the Xperience admin interface.
+    /// </summary>
+    public class AzureOpenAIModule : Module
+    {
+        public const string NAME = "Kentico.Xperience.OpenAI.Azure";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureOpenAIModule"/> class.
+        /// </summary>
+        public AzureOpenAIModule() : base(NAME)
+        {
+        }
+    }
+}
---
+++ src/Kentico.Xperience.OpenAI.Azure/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+using CMS;
+
+[assembly: AssemblyDiscoverable]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a5e9f7b4-c429-44b4-aee8-0f380965934d")]
