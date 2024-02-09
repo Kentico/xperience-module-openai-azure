a9971a9b7f354ba010d34542c81303ba33eb3ab3
Rosalko <robert.zvara.dk@gmail.com>
2024-02-09 15:19:34 +0100

    KX-11170 - Add initial implementation of the Text classification service

---
+++ src/Kentico.Xperience.OpenAI.Azure/Interfaces/ITextClassificationService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using CMS.DocumentEngine;
+
+namespace Kentico.Xperience.OpenAI.Azure
+{
+    public interface ITextClassificationService
+    {
+        IEnumerable<string> ClassifyPage(TreeNode treeNode, IEnumerable<string> categories, CancellationToken cancellationToken);
+
+        string SystemPrompt { get; set; }
+
+        void ResetSystemPrompt();
+
+        bool IsClassificationEnabled { get; }
+    }
+
+
+}
