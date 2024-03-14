75d639a514a10e61c8a1740fc729476ac6b9ada4
michaljakubis <michalj@kentico.com>
2024-03-14 15:39:53 +0100

    test

--- src/DancingGoatCore/ContentItemIdentifiers.cs
+++ src/DancingGoatCore/ContentItemIdentifiers.cs
@@ -3,12 +3,11 @@
     /// <summary>
     /// Encapsulates content item identifiers.
     /// </summary>
-    public static class ContentItemIdentifiers
-    {
+    public static class ContentItemIdentifiers {
         public const string MANUFACTURERS = "/Manufacturers";
         public const string HOME = "/Home";
         public const string CONTACTS = "/Contacts"; 
-        public const string ARTICLES = "/Articles";
+        public const string ARTICLES = "/Articles" ; 
         public const string ABOUT = "/About-Us";
         public const string CAFES = "/Cafes";
         public const string GENERATOR = "/Specials/Generator";
